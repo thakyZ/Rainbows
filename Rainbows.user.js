ef2631c3acecd366ab6d8d429b33200a8cf18bb6
Nire Inicana <thakyZ@users.noreply.github.com>
2016-04-01 14:22:04 -0700

    Possibly fixed script

--- Rainbows.user.js
+++ Rainbows.user.js
@@ -2,9 +2,9 @@
 // @name        Rainbows
 // @namespace   rainbows
 // @version     v0.0.01
+// @icon        https://raw.githubusercontent.com/thakyZ/Rainbows/master/rainbowicon.png
 // @grant       none
 // @require     https://code.jquery.com/jquery-2.2.2.min.js
-// @icon        https://raw.githubusercontent.com/thakyZ/Rainbows/master/rainbowicon.png
 // ==/UserScript==
 
 window.hue = 0;
