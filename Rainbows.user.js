931434031003c5aac3005f856a847af8f0c1e77f
Nire Inicana <thakyZ@users.noreply.github.com>
2016-04-01 14:21:36 -0700

    Update Rainbows.user.js

--- Rainbows.user.js
+++ Rainbows.user.js
@@ -4,7 +4,7 @@
 // @version     v0.0.01
 // @grant       none
 // @require     https://code.jquery.com/jquery-2.2.2.min.js
-// @icon        https://raw.githubusercontent.com/thakyZ/Rainbows/master/rainbowicon.svg
+// @icon        https://raw.githubusercontent.com/thakyZ/Rainbows/master/rainbowicon.png
 // ==/UserScript==
 
 window.hue = 0;
