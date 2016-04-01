ad8681d670dc09be9c2d30b96d52d123980a1f57
Nire Inicana <thakyZ@users.noreply.github.com>
2016-04-01 14:06:45 -0700

    Added v0.0.01 of the rainbows user script

---
+++ Rainbows.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        Rainbows
+// @namespace   rainbows
+// @version     v0.0.01
+// @grant       none
+// @require     https://code.jquery.com/jquery-2.2.2.min.js
+// ==/UserScript==
+
+window.hue = 0;
+window.setInterval(function()
+{
+    $("img").css("filter","hue-rotate("+(window.hue++)+"deg)");
+}, 0.00001);
