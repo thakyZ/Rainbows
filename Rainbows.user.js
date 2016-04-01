b4f8229cae9e41a9330c450c29b30b74e972829b
Nire Inicana <thakyZ@users.noreply.github.com>
2016-04-01 14:20:13 -0700

    Testing to see if svg icons are possible

--- Rainbows.user.js
+++ Rainbows.user.js
@@ -4,6 +4,7 @@
 // @version     v0.0.01
 // @grant       none
 // @require     https://code.jquery.com/jquery-2.2.2.min.js
+// @icon        https://raw.githubusercontent.com/thakyZ/Rainbows/master/rainbowicon.svg
 // ==/UserScript==
 
 window.hue = 0;
